498643a1c27be89f6068430545cafde486c837bf
barronr03 <roberto_geronimo@yahoo.com>
2023-05-07 00:37:37 -0600

    treemaps format

--- Resources/Roberto Treemaps/app.js
+++ Resources/Roberto Treemaps/app.js
@@ -17,7 +17,7 @@
     var genderLabels = ["Female", "Male"]; 
     var genderParents = ["Gender", "Gender"]; 
     
-    var values = [225, 150, 150, 100, 75, 50, 40, 35, 60, 40, 50, 40, 30, 20, 60, 70, 30]; 
+    var values = [100, 0, 25, 60, 55, 10, 0, 50, 60, 40, 0, 40, 30, 20, 60, 0, 85, 65]; 
     var labels = ["VOTER", "Income", "< $40K", "$40-$75K", "$75-$125K", "> $125K", "Education Level", "College", "High school or less", "Some college", "Race", "Black", "Hispanic", "Others", "White", "Gender", "Female", "Male"]; // all labels
     var parents = ["", "VOTER", "Income", "Income", "Income", "Income", "VOTER", "Education Level", "Education Level", "Education Level", "VOTER", "Race", "Race", "Race", "Race", "VOTER", "Gender", "Gender"]; // all parents
     
@@ -26,7 +26,10 @@
       values: values,
       labels: labels,
       parents: parents,
-      marker: {colorscale: 'Greens'}
+      marker: {colorscale: [['College', '#17becf'], ['High school or less', '#bcbd22'], ['Some college', '#ff7f0e'], 
+      ['Black', '#1f77b4'], ['Hispanic', '#aec7e8'], ['Others', '#ffbb78'], ['White', '#ff9896'], 
+      ['Female', '#2ca02c'], ['Male', '#d62728'] 
+    ]}
     }];    
   
 Plotly.newPlot('treemap', data)
